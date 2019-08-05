ba21ff223be3f999caca9bb494e2fe33b9f84cb6
Jens Krumsieck <jenso666@live.de>
2019-08-05 09:32:51 +0200

    Beta 2

--- PorphyStruct/Properties/AssemblyInfo.cs
+++ PorphyStruct/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0 Beta 1")]
+[assembly: AssemblyInformationalVersion("1.0.0 Beta 2")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
