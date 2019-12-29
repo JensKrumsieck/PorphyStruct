36ddc5dc33a74da10d2fedc37a0f6c2dea975cfc
Jens Krumsieck <jenso666@live.de>
2019-12-29 16:56:00 +0100

    Starting to Rework Macrocycle Handling

---
+++ Chemistry/Macrocycles/Corrole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorphyStruct.Chemistry.Macrocycles
+{
+    public class Corrole : Macrocycle
+    {
+        public Corrole(List<Atom> Atoms) : base(Atoms) { }
+    }
+}
