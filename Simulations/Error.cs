67c25b5ca261808328c41aa794df44086aba32d4
Jens Krumsieck <jenso666@live.de>
2019-12-29 16:32:54 +0100

    refactor "FromArray" Method, consider renaming Also remove obsolete code

--- Simulations/Error.cs
+++ Simulations/Error.cs
@@ -1,6 +1,6 @@
-﻿using PorphyStruct.Chemistry;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PorphyStruct.Simulations
 {
@@ -24,61 +24,12 @@ namespace PorphyStruct.Simulations
         /// <param name="arr1"></param>
         /// <param name="arr2"></param>
         /// <returns></returns>
-        public static double FromArray(double[] arr1, double[] arr2)
+        public static double FromArray(IEnumerable<double> arr1, IEnumerable<double> arr2)
         {
-            double error = 0;
-            if (arr1.Length == arr2.Length)
-            {
-                for (int i = 0; i < arr2.Length; i++)
-                {
-                    error += FromDouble(arr1[i], arr2[i]);
-                }
-            }
-            else return double.PositiveInfinity;
-            return Math.Sqrt(error) / arr2.Length;
+            //if unequal lenght->infinity
+            if (arr1.Count() != arr2.Count())
+                return double.PositiveInfinity;
+            return Math.Sqrt(arr1.Zip(arr2, (a, b) => FromDouble(a, b)).Sum()) / arr1.Count();
         }
-
-        /// <summary>
-        /// returns the error of two AtomDataPoints
-        /// </summary>
-        /// <param name="x1"></param>
-        /// <param name="x2"></param>
-        /// <returns></returns>
-        public static double FromDataPoint(AtomDataPoint adp1, AtomDataPoint adp2)
-        {
-            return FromDouble(adp1.Y, adp2.Y);
-        }
-
-        /// <summary>
-        /// Returns the error of to Datapoint lists
-        /// </summary>
-        /// <param name="al1"></param>
-        /// <param name="al2"></param>
-        /// <returns></returns>
-        public static double FromDataPointList(List<AtomDataPoint> al1, List<AtomDataPoint> al2)
-        {
-            double error = 0;
-            if (al1.Count == al2.Count)
-            {
-                for (int i = 0; i < al2.Count; i++)
-                {
-                    error += FromDataPoint(al1[i], al2[i]);
-                }
-            }
-            else return double.PositiveInfinity;
-            return Math.Sqrt(error) / al1.Count;
-        }
-
-        /// <summary>
-        /// Returns the error of two macrocycles
-        /// </summary>
-        /// <param name="cy1"></param>
-        /// <param name="cy2"></param>
-        /// <returns></returns>
-        public static double FromCycle(Macrocycle cy1, Macrocycle cy2)
-        {
-            return FromDataPointList(cy1.dataPoints, cy2.dataPoints);
-        }
-
     }
 }
