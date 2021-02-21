4a9877d820fdd55de02676100f4cd00d706f8cf2
Jens Krumsieck <jenso666@live.de>
2021-02-21 17:32:59 +0100

    add execption handler

--- PorphyStruct.WPF/App.xaml.cs
+++ PorphyStruct.WPF/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace PorphyStruct.WPF
 {
@@ -7,5 +9,18 @@ namespace PorphyStruct.WPF
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Handles Unhandled Exception
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unexpected exception has occurred. Shutting down the application. \nMessage: " + e.Exception.Message, "Unhandled Exception",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+            // Prevent default unhandled exception processing
+            e.Handled = true;
+            Environment.Exit(0);
+        }
     }
 }
