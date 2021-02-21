3e894a6c49ee2c5c6c0f64b36170bd3f8de08326
Jens Krumsieck <jenso666@live.de>
2021-02-21 22:13:45 +0100

    add converter for boolean and

---
+++ PorphyStruct.WPF/Converter/MultiBooleanAndConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace PorphyStruct.WPF.Converter
+{
+    public class MultiBooleanAndConverter : MarkupExtension, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture) => values.OfType<IConvertible>().All(System.Convert.ToBoolean);
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) => throw new NotSupportedException();
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
