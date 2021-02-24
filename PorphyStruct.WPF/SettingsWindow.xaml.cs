4d28cb08234178b4aa4989f7602b9a49e4f291a0
Jens Krumsieck <jenso666@live.de>
2021-02-24 11:40:40 +0100

    Select Path

--- PorphyStruct.WPF/SettingsWindow.xaml.cs
+++ PorphyStruct.WPF/SettingsWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using PorphyStruct.Core;
+﻿using System;
+using System.IO;
+using PorphyStruct.Core;
 using PorphyStruct.ViewModel;
 using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
 using ThemeCommons.Controls;
 
 namespace PorphyStruct.WPF
@@ -17,5 +21,22 @@ namespace PorphyStruct.WPF
         }
 
         private void Save_OnClick(object sender, RoutedEventArgs e) => Settings.Instance.Save();
+
+        private void Search_OnClick(object sender, RoutedEventArgs e)
+        {
+            var btn = (sender as Button)?.Tag.ToString()!;
+            var textBox = (FindName($"{btn}Path") as TextBox);
+            var ofd = new OpenFileDialog()
+            {
+                FileName = "Select Folder.",
+                InitialDirectory = textBox?.Text ?? Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                ValidateNames = false,
+                CheckFileExists = false,
+                CheckPathExists = false
+            };
+            if (ofd.ShowDialog(this) != true) return;
+            textBox!.Text = Path.GetDirectoryName(ofd.FileName)??"";
+            textBox?.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
+        }
     }
 }
