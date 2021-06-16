7e0ae258e187292ba485935983381c5525b60aeb
Jens Krumsieck <jenso666@live.de>
2021-06-16 13:53:44 +0200

    add pdb filter

--- PorphyStruct.WPF/Constants.cs
+++ PorphyStruct.WPF/Constants.cs
@@ -3,10 +3,11 @@
     public static class Constants
     {
         public const string OpenFileFilter =
-            "All Molecule Files|*.cif;*.xyz;*.mol2;*.cdxml;|" +
+            "All Molecule Files|*.cif;*.xyz;*.mol2;*.cdxml;*.pdb;|" +
             "Crystallographic Information File (*.cif)|*.cif|" +
             "XYZ File (*.xyz)|*.xyz|" +
             "Tripos Mol2 (*.mol2)|*.mol|" +
+            "Protein Data Bank (*.pdb)|*.pdb|" +
             "All Files|*.*";
 
         public const string CompareFileFilter =
