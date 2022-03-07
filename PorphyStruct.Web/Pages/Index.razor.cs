90b28147593b84b25b1c2d24b8fce058879451a5
Jens Krumsieck <jenso666@live.de>
2022-03-07 16:31:16 +0100

    Use better FileUpload

--- PorphyStruct.Web/Pages/Index.razor.cs
+++ PorphyStruct.Web/Pages/Index.razor.cs
@@ -1,11 +1,11 @@
-﻿using ChemSharp.Molecules.Blazor;
+﻿using ChemSharp.Molecules;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.JSInterop;
 using PorphyStruct.Core;
 using PorphyStruct.ViewModel;
 using PorphyStruct.ViewModel.IO;
 using PorphyStruct.Web.Shared;
+using Tewr.Blazor.FileReader;
 
 namespace PorphyStruct.Web.Pages;
 
@@ -13,21 +13,47 @@ public partial class Index
 {
     public MacrocycleViewModel? DataContext;
     public TabControl Tabs;
+    private ElementReference inputFile;
+    private bool uploadBusy;
 
     [Inject]
     IJSRuntime JSRuntime { get; set; }
+
+    [Inject]
+    IFileReaderService fileReaderService { get; set; }
+
+    private decimal UploadValue;
 
     protected override void OnInitialized()
     {
         Settings.Instance.Font = "Montserrat";
         base.OnInitialized();
     }
-    async Task OnFileChange(InputFileChangeEventArgs args)
+    async Task OnFileChange()
     {
-        var file = args.File;
-        var molecule = await BlazorMoleculeFactory.CreateAsync(file, 81920000L);
-        var cycle = new Macrocycle(molecule.AtomDataProvider) { Title = file.Name };
-        DataContext = new(cycle);
+        UploadValue = 0;
+        uploadBusy = true;
+        Molecule molecule;
+        DataContext = null;
+
+        foreach (var file in await fileReaderService.CreateReference(inputFile).EnumerateFilesAsync())
+        {
+            var fileInfo = await file.ReadFileInfoAsync();
+            fileInfo.PositionInfo.PositionChanged += (s, e) =>
+            {
+                if (e.PercentageDeltaSinceAcknowledge < 2) return;
+                UploadValue = e.Percentage;
+                InvokeAsync(StateHasChanged);
+                e.Acknowledge();
+            };
+            using MemoryStream memoryStream = await file.CreateMemoryStreamAsync(4096);
+            molecule = await MoleculeFactory.CreateFromStreamAsync(memoryStream, "cif");
+            var cycle = new Macrocycle(molecule.AtomDataProvider) { Title = fileInfo.Name};            
+            DataContext = new(cycle);
+            break; //stop on first
+        }
+        uploadBusy = false;
+        await fileReaderService.CreateReference(inputFile).ClearValue();
     }
 
     async Task OnAnalyzeClick() => await DataContext?.Analyze()!;
--- PorphyStruct.Web/Program.cs
+++ PorphyStruct.Web/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PorphyStruct.Web;
+using Tewr.Blazor.FileReader;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
 await builder.Build().RunAsync();
