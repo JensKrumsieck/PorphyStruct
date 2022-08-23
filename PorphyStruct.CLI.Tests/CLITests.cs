2d4fd3f8fcb31427900f66d92bf3513600f1dfaa
Jens Krumsieck <jenso666@live.de>
2022-08-23 16:29:32 +0200

    change device

--- PorphyStruct.CLI.Tests/CLITests.cs
+++ PorphyStruct.CLI.Tests/CLITests.cs
@@ -1,6 +1,20 @@
-﻿namespace PorphyStruct.CLI.Tests;
+﻿using System.Diagnostics;
+using CommandDotNet;
+using CommandDotNet.TestTools;
+using FluentAssertions;
+using Xunit;
+
+namespace PorphyStruct.CLI.Tests;
 
 public class CLITests
 {
-    
+    [Fact]
+    public void Analyze_ShouldNotThrow()
+    {
+        var result = new AppRunner<Program>()
+            .RunInMem(
+                                                       "analyze testfiles/oriluy.cif -t Porphyrin");
+        result.ExitCode.Should().Be(0);
+        Debug.Write(result.Console.Out);
+    }
 }
--- PorphyStruct.CLI/Program.cs
+++ PorphyStruct.CLI/Program.cs
@@ -1,22 +1,47 @@
 ﻿using CommandDotNet;
+using CommandDotNet.Diagnostics;
+using ConsoleTables;
+using PorphyStruct.Core;
 using PorphyStruct.Core.Analysis;
+using PorphyStruct.ViewModel;
 
 namespace PorphyStruct.CLI;
 
 public class Program
 {
-    public static int Main(string[] args) => new AppRunner<Program>().Run(args);
-
-    // ReSharper disable once UnusedMember.Global
-    [DefaultCommand]
-    public static void Analyze(
-        [Operand(Description = "File to Analyze")] string file,
-        [Named('t', "type", Description = "The Macrocycle's type")]
-        string rawType
-        )
+    static int Main(string[] args)
     {
-        var type = Enum.Parse(typeof(MacrocycleType), rawType);
-        Console.WriteLine(file);
-        Console.WriteLine(type.ToString());
+        Debugger.AttachIfDebugDirective(args);
+        return new AppRunner<Program>().Run(args);
+    }
+
+    [Command("analyze")]
+    public void Analyze(
+        IConsole console,
+        [Positional(Description = "Input file to analyze")]
+        string file,
+        [Option('t', "type", Description = "Macrocyclic Type")]
+        string rawType)
+    {
+        var type = (MacrocycleType)Enum.Parse(typeof(MacrocycleType), rawType);
+        var macrocycle = new Macrocycle(file)
+        {
+            MacrocycleType = type
+        };
+        var viewModel = new MacrocycleViewModel(macrocycle);
+        Task.Run(async () =>
+        {
+            await viewModel.Analyze();
+            console.WriteLine($"{viewModel.Items.Count} {type}s found!");
+            foreach (var item in viewModel.Items)
+            {
+                var result = item.Analysis.Properties?.Simulation.SimulationResult;
+                var headers = result?.Select(s => s.Key).ToArray();
+                var values = result?.Select(s => s.Value).ToArray();
+                var table = new ConsoleTable(headers);
+                table.AddRow(values);
+                table.Write();
+            }
+        });
     }
 }
