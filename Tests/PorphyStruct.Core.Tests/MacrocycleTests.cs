0474a7059969dd1b9cfc11a4a60c088a806ca63f
Jens Krumsieck <jenso666@live.de>
2022-09-03 12:07:31 +0200

    Add new tests

---
+++ Tests/PorphyStruct.Core.Tests/MacrocycleTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using PorphyStruct.Core.Analysis;
+using Xunit;
+
+namespace PorphyStruct.Core.Tests;
+
+public class MacrocycleTests
+{
+    private static readonly Dictionary<MacrocycleType, int> RingSize = new()
+    {
+        { MacrocycleType.Porphyrin, 24 },
+        { MacrocycleType.Corrole, 23 },
+        { MacrocycleType.Norcorrole, 22 },
+        { MacrocycleType.Porphycene, 24 },
+        { MacrocycleType.Corrphycene, 24 }
+    };
+    
+    public static IEnumerable<object[]> GetMacrocycles()
+    {
+        yield return new object[] {"files/830942.cif", MacrocycleType.Corrole};
+        yield return new object[] {"files/NidPTETMP.cif", MacrocycleType.Porphyrin};
+        yield return new object[] {"files/Zn(py)TNPCP.cif", MacrocycleType.Porphyrin};
+        yield return new object[] {"files/oriluy.cif", MacrocycleType.Porphyrin}; 
+        yield return new object[] {"files/CuHETMP.cif", MacrocycleType.Porphyrin};
+    }
+
+    [Theory]
+    [MemberData(nameof(GetMacrocycles))]
+    public void Macrocycle_CanBeCreated(string path, MacrocycleType type)
+    {
+        var cycle = new Macrocycle(path) {MacrocycleType = type};
+        cycle.Should().NotBeNull();
+        cycle.Atoms.Should().HaveCountGreaterThan(0);
+        cycle.Bonds.Should().HaveCountGreaterThan(0);
+        cycle.MacrocycleType.Should().Be(type);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetMacrocycles))]
+    public async Task Macrocycle_CanDetect(string path, MacrocycleType type)
+    {
+        var cycle = new Macrocycle(path) {MacrocycleType = type};
+        await cycle.Detect();
+        cycle.DetectedParts.Should().HaveCountGreaterThan(0);
+        foreach (var p in cycle.DetectedParts)
+            p.Atoms.Should().HaveCount(RingSize[type]);
+    }
+
+    [Fact]
+    public async Task All_Corroles_WillBeDetected()
+    {
+        var cycle = new Macrocycle("files/830942.cif") {MacrocycleType = MacrocycleType.Corrole};
+        await cycle.Detect();
+        cycle.DetectedParts.Should().HaveCount(4);
+    }
+}
