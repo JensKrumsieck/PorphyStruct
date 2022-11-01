9d37d7c1d31feee33bd05958d2b98c8de2961e08
Jens Krumsieck <jenso666@live.de>
2022-11-01 10:07:42 +0100

    fix p as metal

--- PorphyStruct.Core/Extension/ElementUtil.cs
+++ PorphyStruct.Core/Extension/ElementUtil.cs
@@ -6,6 +6,6 @@ public static class ElementUtil
 {
     public static bool CanBeRingMember(this Element e) => e.IsNonMetal || e.IsMetalloid;
 
-    public static bool CanBeCenterAtom(this Element e) => e.IsMetal || e.IsMetalloid;
+    public static bool CanBeCenterAtom(this Element e) => e.IsMetal || e.IsMetalloid || e.Symbol == "P";
 }
 
