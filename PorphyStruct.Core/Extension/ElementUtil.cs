04df0a2a90ca52c122e206c648d2ce4effacbce5
Jens Krumsieck <j.krumsieck@outlook.de>
2025-04-08 17:59:35 +0200

    support platinacorrole

--- PorphyStruct.Core/Extension/ElementUtil.cs
+++ PorphyStruct.Core/Extension/ElementUtil.cs
@@ -4,7 +4,7 @@ namespace PorphyStruct.Core.Extension;
 
 public static class ElementUtil
 {
-    public static bool CanBeRingMember(this Element e) => e.IsNonMetal || e.IsMetalloid;
+    public static bool CanBeRingMember(this Element e) => e.IsNonMetal || e.IsMetalloid || e.Symbol == "Pt";
 
     public static bool CanBeCenterAtom(this Element e) => e.IsMetal || e.IsMetalloid || e.Symbol == "P";
 }
