c5b9854060a240b0851fe9b644bb4820c8bceee7
Jens Krumsieck <j.krumsieck@outlook.de>
2025-06-24 17:28:19 +0200

    added astro

---
+++ docs/astro.config.mjs
@@ -0,0 +1,44 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://porphystruct.org',
+	integrations: [
+		starlight({
+			title: 'PorphyStruct',
+			customCss: [
+				'./src/styles/global.css',
+			],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/jenskrumsieck/porphystruct' }],
+			logo: {
+				src: './src/assets/logo.svg'
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			components: {
+				Hero: './src/components/Hero.astro',
+			}
+		}),
+		sitemap()
+	],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
